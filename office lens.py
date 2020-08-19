d76197c70d87b051d3627dcdbb917761827ea0db
Shubh-coder007 <shubhdholakiya>
2020-08-19 11:09:02 +0530

    First Git Commit: YT Tags

---
+++ office lens.py
@@ -0,0 +1,8 @@
+with open("Office_lens.txt", 'w') as lens_file:
+    print("lens, microsoft office lens,"
+          " office lens, microsoft lens,"
+          " office lens app, microsoft office lens app,"
+          " office lens download, office lens, microsoft lens,"
+          " microsoft office lens, scanner,"
+          " camscanner, office lens app", file=lens_file)
+
